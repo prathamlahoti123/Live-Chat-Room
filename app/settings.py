ef323b0f8ae35a0bd59082bf865a28a4e8f0e4e8
prathamlahoti123 <prathamlahoti@duck.com>
2025-07-25 14:23:29 +0300

    Add HOST and PORT settings

--- app/settings.py
+++ app/settings.py
@@ -8,6 +8,8 @@ class Config:
   SECRET_KEY = os.getenv("SECRET_KEY", secrets.token_urlsafe(24))
   DEBUG = os.getenv("FLASK_DEBUG", "False").lower() in ("true", "1", "t")
   CORS_ORIGINS = os.getenv("CORS_ORIGINS", "*")
+  HOST: str = os.getenv("HOST", "0.0.0.0")
+  PORT: int = int(os.getenv("PORT", "5000"))
 
   # Available chat rooms - stored as constant for now, could be moved to database
   CHAT_ROOMS = ["General", "Zero to Knowing", "Code with Josh", "The Nerd Nook"]
