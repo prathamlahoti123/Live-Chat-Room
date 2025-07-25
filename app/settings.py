d599eec5b34cfd1e525b5e23e51569b8746f7861
prathamlahoti123 <prathamlahoti@duck.com>
2025-07-25 14:09:28 +0300

    Replace os.environ.get with os.getenv

--- app/settings.py
+++ app/settings.py
@@ -5,9 +5,9 @@ import secrets
 class Config:
   """Application configuration"""
 
-  SECRET_KEY = os.environ.get("SECRET_KEY", secrets.token_urlsafe(24))
-  DEBUG = os.environ.get("FLASK_DEBUG", "False").lower() in ("true", "1", "t")
-  CORS_ORIGINS = os.environ.get("CORS_ORIGINS", "*")
+  SECRET_KEY = os.getenv("SECRET_KEY", secrets.token_urlsafe(24))
+  DEBUG = os.getenv("FLASK_DEBUG", "False").lower() in ("true", "1", "t")
+  CORS_ORIGINS = os.getenv("CORS_ORIGINS", "*")
 
   # Available chat rooms - stored as constant for now, could be moved to database
   CHAT_ROOMS = ["General", "Zero to Knowing", "Code with Josh", "The Nerd Nook"]
