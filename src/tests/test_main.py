fc93484a7ee3129e2ea309d78593f089b9e0cb00
prathamlahoti123 <prathamlahoti@duck.com>
2025-09-13 22:44:16 +0300

    Add test for healthcheck route

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,2 +1,11 @@
-def test() -> None:
-  assert True
+from http import HTTPStatus
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+  from flask.testing import FlaskClient
+
+
+def test_health(http_client: "FlaskClient") -> None:
+  resp = http_client.get("/health")
+  assert resp.status_code == HTTPStatus.NO_CONTENT
+  assert resp.headers["x-status"] == "ok"
