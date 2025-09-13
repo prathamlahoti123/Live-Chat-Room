acdf69b66ba4717ce007da87f5d4f0866e0c50e1
prathamlahoti123 <prathamlahoti@duck.com>
2025-09-13 22:45:22 +0300

    Update env variables to specify host and port of the main app

--- proxy/run.sh
+++ proxy/run.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-envsubst '${NGINX_PORT},${NGINX_HOST},${APP_PORT}' \
+envsubst '${NGINX_HOST},${NGINX_PORT},${FLASK_HOST},${FLASK_PORT}' \
   < /etc/nginx/default.conf.tpl \
   > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
