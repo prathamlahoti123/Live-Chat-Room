ad0da2a1f83f1f4ca2c02446fc18ddd38cbb5a7c
prathamlahoti123 <prathamlahoti@duck.com>
2025-09-15 11:20:20 +0300

    Update fixture to mock Flask's session

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -17,7 +17,37 @@ def http_client() -> Iterator["FlaskClient"]:
     yield client
 
 
-@pytest.fixture(scope="session")
-def socket_client(http_client: "FlaskClient") -> "SocketIOTestClient":
+@pytest.fixture
+def socket_client() -> "SocketIOTestClient":
   """Fixture to provide a Flask-SocketIO test client."""
-  return socketio.test_client(app, flask_test_client=http_client)
+  return socketio.test_client(app)
+
+
+# @pytest.fixture(scope="session")
+# def mock_session() -> Iterator[dict[str, str]]:
+#   """Fixture to mock the Flask session."""
+#   with patch("app.main.session", {}) as session:
+#     yield session
+
+
+# @pytest.fixture(scope="session")
+# def monkeysession() -> Iterator[pytest.MonkeyPatch]:
+#   """Session-scoped MonkeyPatch fixture."""
+#   with pytest.MonkeyPatch.context() as mp:
+#     yield mp
+
+
+# @pytest.fixture(scope="session")
+# def mock_session(monkeysession: pytest.MonkeyPatch) -> dict[str, str]:
+#   """Fixture to mock the Flask session."""
+#   session: dict[str, str] = {}
+#   monkeysession.setattr("app.main.session", session)
+#   return session
+
+
+@pytest.fixture
+def mock_session(monkeypatch: pytest.MonkeyPatch) -> dict[str, str]:
+  """Fixture to mock the Flask session."""
+  session: dict[str, str] = {}
+  monkeypatch.setattr("app.main.session", session)
+  return session
